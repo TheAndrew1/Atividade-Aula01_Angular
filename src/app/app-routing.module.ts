a967d4b48aeac64b07ad8a8b5e40e18eca0e9a65
andre <andremaivelasco@hotmail.com>
2023-10-03 23:10:20 -0300

    lista com if e componentes no index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListaComponent } from './lista/lista.component';
+import { IndexComponent } from './index/index.component';
 
 const routes: Routes = [
-  {path: "", component: ListaComponent}
+  {path: "", component: IndexComponent}
 ];
 
 @NgModule({
--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -10,10 +10,16 @@ export class ListaComponent {
   pessoas: Pessoa[] = [];
 
   constructor(){
-    this.pessoas = [new Pessoa("Andre", 21),
+    this.pessoas = [
+      new Pessoa("Andre", 21),
       new Pessoa("Frederico", 21),
       new Pessoa("Andrei", 19),
       new Pessoa("Cleyton", 25),
-      new Pessoa("Véio", 35)]
+      new Pessoa("João", 35),
+      new Pessoa("Luis", 27),
+      new Pessoa("Julia", 23),
+      new Pessoa("Jorge", 81),
+      new Pessoa("Maria", 30),
+      new Pessoa("Cleber", 18)]
   }
 }
