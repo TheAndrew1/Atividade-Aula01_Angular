0e9132f2b32f2714913c1126cf5d6f92f79ecafd
Andre Mai Velasco <andremaivelasco@hotmail.com>
2023-10-07 23:41:25 -0300

    arrumado adicionar pessoa

--- src/app/pessoa/pessoa-detail/pessoa-detail.component.ts
+++ src/app/pessoa/pessoa-detail/pessoa-detail.component.ts
@@ -1,16 +1,24 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { Pessoa } from '../pessoaModel';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-pessoa-detail',
   templateUrl: './pessoa-detail.component.html',
   styleUrls: ['./pessoa-detail.component.scss']
 })
-export class PessoaDetailComponent {
+export class PessoaDetailComponent{
+  modalService = inject(NgbModal);
   @Input() pessoa: Pessoa = new Pessoa();
   @Output() retorno = new EventEmitter<Pessoa>();
 
   cadastrar(){
-    this.retorno.emit(this.pessoa);
+    if(this.pessoa.nome != null && this.pessoa.nome != "" && this.pessoa.idade != null && this.pessoa.idade > 0){
+      this.retorno.emit(this.pessoa);
+    }else{
+      alert("Nome e/ou idade inválida!");
+    }
+    
+    this.modalService.dismissAll();
   }
 }
--- src/app/pessoa/pessoaModel.ts
+++ src/app/pessoa/pessoaModel.ts
@@ -1,5 +1,4 @@
 export class Pessoa{
-    id!: number;
     nome!: string;
     idade!: number
 }
