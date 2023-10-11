301157af347594661dcf85e8384e7e35a8663088
andre <andremaivelasco@hotmail.com>
2023-10-10 21:11:08 -0300

    service pessoa feita

---
+++ src/app/pessoa/pessoa.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Pessoa } from './pessoaModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PessoaService {
+  API: string = "http://localhost:8080/api/pessoa";
+  http = inject(HttpClient);
+
+  constructor() { }
+
+  listAll(): Observable<Pessoa[]> {
+    return this.http.get<Pessoa[]>(this.API); 
+  }
+
+  save(pessoa: Pessoa): Observable<Pessoa> {
+    return this.http.post<Pessoa>(this.API, pessoa);
+  }
+
+  edit(id: number, pessoa: Pessoa): Observable<Pessoa> {
+    return this.http.put<Pessoa>(this.API+"?id="+id, pessoa);
+  }
+
+  delete(id: number): Observable<Pessoa> {
+    return this.http.delete<Pessoa>(this.API+"?id="+id);
+  }
+
+  exemploErro(): Observable<Pessoa[]> {
+    return this.http.get<Pessoa[]>(this.API + '/erro');
+  }
+}
